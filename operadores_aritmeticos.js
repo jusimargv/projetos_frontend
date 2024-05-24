015be96cc0af0bdd15f6b5bb5717c435f2c28b2a
jusimargv <jusimargv@gmail.comgit config --global user.email jusimargv@gmail.com>
2024-05-23 21:00:20 -0300

    Doc_update

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,25 @@
+let primeiroExemplo = 2 + 5 + 8;
+console.log(primeiroExemplo); // 15
+
+let segundoExemplo = 8 + 3 * 4;
+console.log(segundoExemplo); // 20
+
+let terceiroExemplo = (4 + 5 - 3) / 2;
+console.log(terceiroExemplo ); // 3
+
+let quartoExemplo = 2 ** 3;
+console.log(quartoExemplo); // 8
+
+let quintoExemplo = 10 % 7;
+console.log(quintoExemplo); // 3
+
+let meuUltimoExemplo = primeiroExemplo + segundoExemplo * (terceiroExemplo / quintoExemplo) - quartoExemplo;
+console.log(meuUltimoExemplo ); // 27
+
+let hello = 'Hello';
+hello = hello + ' ' + 'World';
+
+console.log(hello); // "Hello World"
+
+let somaBoolean = true + true;
+console.log(somaBoolean); // 2
---
+++ variaveis.js
@@ -0,0 +1,9 @@
+let nome ="Jusimar";//string
+let idade = 42; // number
+let aprovado = true; // boolean
+
+
+console.log(typeof nome); //string 
+console.log( typeof idade); //number
+console.log( typeof aprovado);// boolean
+
